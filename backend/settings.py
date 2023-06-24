400c741a545a7869044fa8fc302aa92cffd7687a
Manuel Collazo <collazo.manuel.28@gmail.com>
2023-06-24 13:51:35 -0300

    Add settings.py file to use decouple-python logic

---
+++ backend/settings.py
@@ -0,0 +1,7 @@
+import os
+from decouple import config
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+OPENAI_KEY = config('OPENAI_KEY')
+MONGODB_KEY = config('MONGODB_KEY')
+#DEBUG = config('DEBUG', cast=bool)
