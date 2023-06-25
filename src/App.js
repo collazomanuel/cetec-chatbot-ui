9e1f20e81d46aff982138c37b3bf1cd94418f7e9
Manuel Collazo <collazo.manuel.28@gmail.com>
2023-06-25 18:04:26 -0300

    Implement React OAuth2 to log in users

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
+
+import React, { useState, useEffect } from 'react';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 import fiubaLogo from './fiuba-logo.png'
 import './App.css';
 import Chat from './Chat'
 
 function App() {
+
+  const [ user, setUser ] = useState([]);
+  const [ profile, setProfile ] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log('Login Failed: ', error)
+  });
+
+  const logout = () => {
+    googleLogout();
+    setProfile(null);
+};
+
+  useEffect(
+    () => {
+      if (user) {
+        axios
+          .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: 'application/json'
+            }
+          })
+          .then((response) => {
+            setProfile(response.data);
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+    [user]
+  );
+
   return (
     <div className=' App'>
       <div className='App-header'>
         <h2><code>CETEC Chatbot</code></h2>
       </div>
-      <div className='chatbot'>
-        <Chat />
-      </div>
-      <div className='App-footer'>
-        <img src={fiubaLogo} className='fiuba-logo' alt='fiuba-logo' />
-      </div>
+      {profile ? (
+        <>
+          <div className='chatbot'>
+            <Chat />
+          </div>
+          <button onClick={logout}>Cerrar sesión</button>
+        </>
+      ) : (
+        <>
+          <button onClick={() => login()}>Iniciar sesión con Google</button>
+          <div className='App-footer'>
+            <img src={fiubaLogo} className='fiuba-logo' alt='fiuba-logo' />
+          </div>
+        </>        
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="254003460922-0o6c7ejs39uhjdp76kcgk1uldgui7sfh.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />    
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
