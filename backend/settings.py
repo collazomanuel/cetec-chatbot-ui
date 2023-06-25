a7a3dbcc880812f1ab4523940302eeae45a53a17
Manuel Collazo <collazo.manuel.28@gmail.com>
2023-06-25 17:59:41 -0300

    Remove "DEBUG" from settings.py file

--- backend/settings.py
+++ backend/settings.py
@@ -4,4 +4,3 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 OPENAI_KEY = config('OPENAI_KEY')
 MONGODB_KEY = config('MONGODB_KEY')
-#DEBUG = config('DEBUG', cast=bool)
